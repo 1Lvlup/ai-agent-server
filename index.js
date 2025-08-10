5029bdb1cc11afff41ccb6a69abd46ae91e38834
1LvlUp <108124741+1Lvlup@users.noreply.github.com>
2025-08-10 17:28:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import http from 'http';
-import { WebSocketServer } from 'ws';
+import WebSocket, { WebSocketServer } from 'ws';
 import fetch from 'node-fetch';
 
 const app = express();
